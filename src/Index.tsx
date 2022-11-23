26712d419981fbd43175f6bad6a1e34b81df75bd
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-11-23 21:12:31 +0500

    feat: add css-loader style-loader, html-webpack-plugin, clean-webpack

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './styles/style.css'
+
 const rootNode = document.getElementById('app');
 if(rootNode){
   createRoot(rootNode)
